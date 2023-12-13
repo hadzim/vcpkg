be7a2d0db23dc80514b19c6ad3f77e99c7fa9866
Vilem Jelen <vilem.jelen@tbs-biometrics.com>
2023-12-13 09:51:42 +0100

    update vcpkg install locations

--- copy-external.bat
+++ copy-external.bat
@@ -1,2 +1,2 @@
-XCOPY "external\x86-windows\*" "vcpkg_installed\x86-windows\"  /s /i /Y
-XCOPY "external\x64-windows\*" "vcpkg_installed\x64-windows\"  /s /i /Y
+XCOPY "external\x86-windows\*" "vcpkg_installed_x86\x86-windows\"  /s /i /Y
+XCOPY "external\x64-windows\*" "vcpkg_installed_x64\x64-windows\"  /s /i /Y
--- update.bat
+++ update.bat
@@ -1,2 +1,2 @@
-vcpkg install --triplet x86-windows
-vcpkg install --triplet x64-windows
+vcpkg install --triplet x86-windows --x-install-root vcpkg_installed_x86
+vcpkg install --triplet x64-windows --x-install-root vcpkg_installed_x64
